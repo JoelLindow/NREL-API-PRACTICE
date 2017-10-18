107dc6e48cbd8f9a7bc343bc33c1722b48f22097
JoelLindow <joellindow@gmail.com>
2017-10-18 10:24:55 -0600

    parsed out required parameters for distance and limit. Also passed in params to variable for zip code. Also stashed away API key and called ENVirinmental variable

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,9 @@ class SearchController < ApplicationController
     radius = 5.0
     zip = params["q"]
 
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&state=CO&radius=5.0&zip=80203&limit=5&api_key=nuHt8BQdoOEbMl3sWRKXTOYNjGtEfFW814oWQNyE&format=JSON")
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=#{fuel_types}&state=CO&radius=#{radius}&zip=#{zip}&limit=5&api_key=#{ENV["NREL_KEY"]}&format=JSON")
 
-  # binding.pry
+  binding.pry
 
 
 
