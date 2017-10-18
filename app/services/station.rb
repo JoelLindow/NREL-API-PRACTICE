38ca9c0af5afaed662eb350a7e7e253d016ff84b
JoelLindow <joellindow@gmail.com>
2017-10-18 11:03:25 -0600

    building view to return search data

--- app/services/station.rb
+++ app/services/station.rb
@@ -1,7 +1,11 @@
 class Station
 
   def initialize(station_data = {})
-    binding.pry
+    @name         = station_data[:station_name]
+    @address      = station_data[:street_address]
+    @fuel_types   = station_data[:fuel_type_code]
+    # @distance     = station_data[]
+    @access_times = station_data[:access_days_time]
   end
 
 end
