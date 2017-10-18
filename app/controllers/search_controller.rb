a166c4a712665ef6ccd641c5240de5b4f5fff9e7
JoelLindow <joellindow@gmail.com>
2017-10-18 10:19:50 -0600

    having oruble with VCR clogging up my testing. Going to move forward with controller to get bare bones functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
-    
+    fuel_types = "LPG,ELEC"
+    radius = 5.0
+    zip = params["q"]
+
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&state=CO&radius=5.0&zip=80203&limit=5&api_key=nuHt8BQdoOEbMl3sWRKXTOYNjGtEfFW814oWQNyE&format=JSON")
+
+  # binding.pry
+
+
+
   end
 end
