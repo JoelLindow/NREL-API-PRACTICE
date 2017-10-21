f99a1c39ac88560b39280ad2adfe1d86ae733bf1
JoelLindow <joellindow@gmail.com>
2017-10-21 11:53:51 -0600

    another refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,20 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @stations = StationsService.new.search_by_zip(params[:q])
-
-    # request = Faraday.get "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?format=JSON" do |faraday|
-    #   faraday.params[:fuel_type] = "LPG,ELEC"
-    #   faraday.params[:state] = "CO"
-    #   faraday.params[:radius] = 6.0
-    #   faraday.params[:location] = params[:q]
-    #   faraday.params[:limit] = 10
-    #   faraday.params[:api_key] = ENV["NREL_KEY"]
-    # end
-    #
-    # raw_stations = JSON.parse(request.body, symbolize_names: true)[:fuel_stations]
-    #
-    # @stations = raw_stations.map do |station|
-    #   Station.new(station)
-    # end
+    @stations = StationsService.new.api_call(params[:q])
   end
 end
